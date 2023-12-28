a536971320115887b2fc84c40c3c77723d7cd214
m4chado <enzo653fm@gmail.com>
2023-12-28 14:08:09 -0300

    feat: add passportjs and jwt module with nestjs

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -8,10 +8,16 @@ import * as bcrypt from 'bcrypt';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto, RegisterDto } from './dto';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable({})
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
 
   async signup({ name, email, password }: RegisterDto) {
     const passwordHash = await bcrypt.hash(password, 6);
@@ -25,9 +31,7 @@ export class AuthService {
         },
       });
 
-      delete user.passwordHash;
-
-      return { user };
+      return this.signToken(user.id);
     } catch (error) {
       if (error instanceof PrismaClientKnownRequestError) {
         if (error.code === 'P2002') {
@@ -50,8 +54,21 @@ export class AuthService {
 
     if (!passowordMatches) throw new BadRequestException('Invalid credentials');
 
-    delete user.passwordHash;
+    return await this.signToken(user.id);
+  }
 
-    return { user };
+  async signToken(userId: number): Promise<{ token: string }> {
+    const payload = {
+      sub: userId,
+    };
+
+    const secret = this.config.get('JWT_SECRET');
+
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '15m',
+      secret,
+    });
+
+    return { token };
   }
 }
---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,13 @@
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(config: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: config.get('JWT_SECRET'),
+    });
+  }
+}
