8ac79904e088037befe3c408a4679d63a2d418bf
m4chado <enzo653fm@gmail.com>
2023-12-28 11:44:42 -0300

    feat: create sign-up logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable({})
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  signup(dto: AuthDto) {
-    return { dto };
+  async signup({ name, email, password }: AuthDto) {
+    const passwordHash = await bcrypt.hash(password, 6);
+
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          name,
+          email,
+          passwordHash,
+        },
+      });
+
+      delete user.passwordHash;
+
+      return { user };
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ConflictException('E-mail is already beign used');
+        }
+      }
+
+      throw error;
+    }
   }
 
   signin() {
